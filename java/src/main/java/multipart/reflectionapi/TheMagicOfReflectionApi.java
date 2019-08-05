619bec63a1e4e926d27d42ed947bc0271aee849b
Imran Shaikh <imran@coderstea.com>
2019-08-05 21:16:43 +0530

    Initial commit

---
+++ java/src/main/java/multipart/reflectionapi/TheMagicOfReflectionApi.java
@@ -0,0 +1,111 @@
+package multipart.reflectionapi;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class TheMagicOfReflectionApi {
+
+    public static void main(String[] args) throws Exception {
+        obtainTheClassObject();
+        constructor();
+        _Field();
+        _Method();
+    }
+
+    static void obtainTheClassObject() {
+        Class<Customer> viaClassName = Customer.class;
+        System.out.println("Via class name " + viaClassName.getName());
+
+        Class<? extends Customer> viaObject = new Customer().getClass();
+        System.out.println("Via object " + viaObject.getName());
+
+    }
+
+    static void constructor() throws Exception {
+        Constructor<Customer> customerConstructor = Customer.class.getConstructor();
+
+        Customer cu = customerConstructor.newInstance();
+        System.out.println("No parameterized constructors name will be null: " + cu.getName());
+
+        Constructor<Customer> parameterizedConstructor = Customer.class
+                .getDeclaredConstructor(String.class, int.class);
+        System.out.println("Count of parameters : " + parameterizedConstructor.getParameterCount());
+
+        Customer detailedCustomer = parameterizedConstructor.newInstance("Coders Tea", 1);
+        System.out.println("Lets here it for the : " + detailedCustomer);
+    }
+
+    static void _Field() throws Exception {
+        Customer customer = new Customer("Field", 2);
+
+        Field nameField = customer.getClass().getDeclaredField("name");
+        boolean isNameAccessible = nameField.isAccessible();
+        nameField.setAccessible(true);
+
+        Field idField = customer.getClass().getDeclaredField("id");
+        boolean isIdAccessible = idField.isAccessible();
+        idField.setAccessible(true);
+
+        System.out.println("Name is : " + nameField.get(customer));
+        System.out.println("id is " + idField.getInt(customer));
+
+        nameField.set(customer, "New Name");
+        System.out.println("new Name" +
+                " is : " + nameField.get(customer));
+
+        //make sure to reset accessibility
+        nameField.setAccessible(isNameAccessible);
+        idField.setAccessible(isIdAccessible);
+
+        //to access all the fields
+        for (Field field : Customer.class.getDeclaredFields()) {
+            boolean isFieldAccessible = field.isAccessible();
+            System.out.println("Field being access is : " + field.getName() + " with value : " + field.get(customer));
+
+            field.setAccessible(isFieldAccessible);
+        }
+
+    }
+
+    static void _Method() throws Exception {
+        Customer customer = new Customer();
+        Method setIdMethod = customer.getClass().getMethod("setId");
+        setIdMethod.invoke(customer, 2);
+        System.out.println("Parameters count is " + setIdMethod.getParameterCount());
+
+        Method getIdMethod = customer.getClass().getMethod("getId");
+        int id = (int) getIdMethod.invoke(customer);
+        System.out.println("id is " + id);
+
+    }
+}
+
+class Customer {
+    private String name;
+    private int id;
+
+    public Customer(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Customer() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ java/src/main/java/multipart/stream/JavasFunctionalInterfaces.java
@@ -0,0 +1,118 @@
+package multipart.stream;
+
+import java.util.function.*;
+
+public class JavasFunctionalInterfaces {
+    public static void main(String[] args) {
+        fi_Consumer();
+        fi_Predicate();
+        fi_Function();
+        fi_BiFuntion();
+        fi_Supplier();
+    }
+
+
+    static void fi_Consumer() {
+        //define the consumer which consumes the age to print it
+        Consumer<Integer> printAgeConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer age) {
+                System.out.println("Age is " + age);
+            }
+        };
+
+        //call the method
+        printAgeConsumer.accept(23);// Age is 23
+
+        //using lambda
+        Consumer<Integer> printAgeWithLamda = (age) -> System.out.println("Lamdda : age is " + age);
+        //will work similar as printAgeConsumer
+        printAgeWithLamda.accept(223);//Lamda : age is 223
+
+        //chaining with andThen(Consumer)
+        printAgeConsumer //1st
+                .andThen(printAgeWithLamda)//2nd
+                .andThen(age -> System.out.println("How old is he ? " + age))//3rd
+                .accept(23);//this value will be given to each consumer
+    }
+
+    static void fi_Predicate() {
+        Predicate<Integer> isEven = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer number) {
+                return number % 2 == 0;
+            }
+        };
+        boolean is23Even = isEven.test(23);//false
+
+        //using lambda
+        Predicate<Integer> isEvenlambda = (number) -> number % 2 == 0;
+        boolean is46Even = isEvenlambda.test(46);//true
+
+        //chaining with and()
+        //returns true only of all the predicated returns true else false
+        isEven // 1st - number should be even
+                .and(n -> n < 10)//2nd -number should be less than 10
+                .and(n -> n > 5) // 3rd should be greater than 5
+                .test(6); // this nu,ber will be used in chain
+        // returns true after completion
+    }
+
+    static void fi_Function() {
+        Function<Integer, Integer> doubleTheNumber = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer number) {
+                return number * 2;
+            }
+        };
+        int doubleTheNumber2 = doubleTheNumber.apply(2);//4
+        //using lambda
+        Function<Integer, Integer> subtract2 = (number) -> number - 2;
+        int subtract2From4 = doubleTheNumber.apply(4);
+        System.out.println(subtract2From4);
+
+        //chaining with andThen()
+        //Unlike Predicate and Consumer which uses the same value to all the nodes,
+        //Function uses the result of previous Function
+        doubleTheNumber // 1st will double the number
+                .andThen(subtract2) // 2nd will subtract 2 from doubled number
+                .andThen(doubleTheNumber) // 3rd subtracted result will be doubled
+                .apply(4); // (((4 * 2) - 2) * 2) = 12
+    }
+
+    static void fi_BiFuntion() {
+        BiFunction<Integer, Integer, Integer> areaOfRectangle = new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer length, Integer breadth) {
+                return length * breadth;
+            }
+        };
+
+        areaOfRectangle.apply(2, 2);//4
+
+        BiFunction<Integer, Integer, String> areaWitMessage = (lengh, breadth) -> "Area is " + lengh * breadth;
+
+        areaWitMessage.apply(4, 3);// Area is 12
+
+        //chaining
+        areaOfRectangle // 1st area will be calculated
+                .andThen(area -> area * 3) // 2nd calculated area will be multiplied by 3 as height
+                .apply(2, 3);// 2 * 3 * 3 = 18
+    }
+
+    static void fi_Supplier() {
+        Supplier<Double> randomSupplier = new Supplier<Double>() {
+            @Override
+            public Double get() {
+                return Math.random();
+            }
+        };
+        double randomNumber = randomSupplier.get();
+        System.out.println(randomNumber);
+
+        //lambda
+        Supplier<String> codersTeaUrl = () -> "https://coderstea.com";
+        System.out.println(codersTeaUrl.get());
+    }
+}
+
